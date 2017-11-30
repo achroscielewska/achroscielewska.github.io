8060ee2548b7145100e3865d7ed1d11774582585
achroscielewska <achroscielewska@gmail.com>
2017-11-30 17:29:06 +0100

    Ex2

--- js/script/js.js
+++ js/script/js.js
@@ -1,12 +1,29 @@
 /**Ex 1**/
 function evenOrOdd (evenOrOddInput) {
-    evenOrOddInput = document.getElementById("evenOrOddInput").value;
+    evenOrOddInput = document.getElementById("evenOrOddInput").value * 1;
 
-    // if(!(Number.isInteger(evenOrOddInput))) {
-    //    throw new Error (alert('Radius has to be bigger than 0'));
-    //}
+    if(!(Number.isInteger(evenOrOddInput))) {
+        throw new Error (alert('Imput has to be Integer'));
+    }
 
     return (evenOrOddInput % 2 === 0)
         ? document.getElementById("evenOrOddResult").innerHTML = 'even'
         : document.getElementById("evenOrOddResult").innerHTML = 'odd';
+}
+
+/**Ex 2**/
+function previousAndNext (previousAndNextInput) {
+    previousAndNextInput = document.getElementById("previousAndNextInput").value * 1;
+
+    if(!(Number.isInteger(previousAndNextInput))) {
+        throw new Error (alert('Imput has to be Integer'));
+    }
+
+    var previous, next;
+    previous = previousAndNextInput - 1;
+    next = previousAndNextInput + 1;
+
+    return document.getElementById("previous").innerHTML = previous,
+        document.getElementById("previousAndNextInput").innerHTML = previousAndNextInput,
+        document.getElementById("next").innerHTML = next;
 }

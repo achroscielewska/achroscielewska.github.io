50b779d754ca09e499ffb69e714f4adca0c0d0c2
achroscielewska <achroscielewska@gmail.com>
2017-12-18 12:41:36 +0100

    JS30 | JS30_2 porządki

--- js30/js30_2/script/script.js
+++ js30/js30_2/script/script.js
@@ -6,18 +6,16 @@ function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
-    
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
     const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + 90;
 
-    console.log(seconds);
-
-    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-
     const hour = now.getHours();
     const hourDegrees = ((hour / 12) * 360) + 90;
+
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }

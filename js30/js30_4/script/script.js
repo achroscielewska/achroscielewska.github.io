75bdbb28cfb705ddbaff241df142e887f4f86622
achroscielewska <achroscielewska@gmail.com>
2017-12-19 14:55:19 +0100

    JS30_4 | dane, funkcje

--- js30/js30_4/script/script.js
+++ js30/js30_4/script/script.js
@@ -14,3 +14,50 @@ const inventors = [
 ];
 
 const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
+
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck', 'pogostick'];
+
+// array.prototype.filter() - gives filtered elements
+const fifteen = inventors.filter(inventor => (inventor.year >= 1500 && inventor.year < 1600));
+console.table(fifteen);
+
+// array.prototype.map() - gives all elements
+
+const fullNames = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+console.table(fullNames);
+
+// array.prototype.sort() - gives all elements
+
+const ordered = inventors.sort((a,b) => (a.year > b.year) ? 1 : -1);
+console.table(ordered);
+
+// array.prototype.reduce()
+
+const totalYears = inventors.reduce((total, inventor) => {return total + (inventor.passed - inventor.year);},0);
+console.log(totalYears);
+
+// array.prototype.sort()
+const yearsLived = inventors.sort((a,b) => ((a.passed - a.year) > (b.passed - b.year)) ? -1 : 1);
+console.table(yearsLived);
+
+//array.prototype.sort()
+const alpha = people.sort((a, b) => {
+    const [aLast, aFirst] = a.split(', ');
+    const [bLast, bFirst] = b.split(', ');
+
+    return aLast > bLast ? 1 : -1;
+});
+
+console.log(alpha);
+
+// reduce
+
+const transportation = data.reduce((obj, item) => {
+    if(!obj[item]) {
+        obj[item] = 0;
+    }
+    obj[item]++;
+    return obj;
+}, {});
+
+console.log(transportation);

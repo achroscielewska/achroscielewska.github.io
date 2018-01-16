aaeee2a6742d7b5fa13d323a5f7438e4d04844d0
achroscielewska <achroscielewska@gmail.com>
2018-01-16 19:36:13 +0100

    react-app | dataset dodany i użyty żeby wyświetlać dane na itemCard

--- react-app/src/components/ItemCard.js
+++ react-app/src/components/ItemCard.js
@@ -3,7 +3,7 @@ import {Panel, Button, Col, Label} from "react-bootstrap";
 import img from "../img/champagne-146885_640.png"
 
 const ItemCard = (props) => {
-
+    console.log(props.tags);
     return (
         <Col xs={12} sm={6} md={3}>
             <div className='card'>
@@ -16,21 +16,22 @@ const ItemCard = (props) => {
                     <div></div>
                     <div></div>
                     <div className='description-detalis'>
-                        <b> title </b>
+                        <b> {props.title} </b>
                     </div>
                     <div>
-                        <Label>New</Label>
-                        <Label>New</Label>
-                        <Label>New</Label>
+                        {props.tags.map(tag => (
+                            <Label>{tag}</Label>
+                        ))}
+
                     </div>
                     <div>
                         <Button >Szczegóły</Button>
                     </div>
-
                 </div>
             </div>
         </Col>
     )
+    console.log('this.props')
 };
 
 export default ItemCard;
--- react-app/src/scenes/Home.js
+++ react-app/src/scenes/Home.js
@@ -3,17 +3,25 @@ import {Row, Col } from "react-bootstrap";
 
 import ItemCard from "../components/ItemCard";
 
+import dataset from "../dataset"
+
 const Home = () => (
     <Row className="show-grid">
         <Col xs={12} >
             <h2>Home</h2>
             <hr />
 
-            <ItemCard/>
-            <ItemCard/>
-            <ItemCard/>
-            <ItemCard/>
-            <ItemCard/>
+            {dataset.map(item => (
+                <ItemCard
+                    key={item.id}
+                    title={item.title}
+                    description={item.description}
+                    img={item.img}
+                    date={item.date}
+                    tags={item.tags}
+                    isActive={item.isActive}
+                />
+            ))}
 
         </Col>
     </Row>

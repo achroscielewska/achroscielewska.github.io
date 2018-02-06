e80ff0727ff28c1d3f15b44777e994bd6a7045c7
achroscielewska <achroscielewska@gmail.com>
2018-02-06 13:09:28 +0100

    to-do-list-app | zmiany pod cssy

--- to-do-list-app/src/App.js
+++ to-do-list-app/src/App.js
@@ -1,21 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
+import classes from './index.css';
+
+import {Liczniki} from './scenes/Liczniki';
+import Todo from './scenes/Todo';
+import reducer from "./reducers";
+
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+const store = createStore(reducer);
+
+const App = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+        <Provider store={store}>
+            <div className={classes.Container}>
+                <Todo />
+            </div>
+        </Provider>
+    )
+};
 
 export default App;

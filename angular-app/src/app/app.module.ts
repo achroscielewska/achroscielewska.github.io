e3b5272bdadf2dcf7b58c2568ad2e955f45aa0d0
achroscielewska <achroscielewska@gmail.com>
2018-03-21 16:48:43 +0100

    angular-app | recips components

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
 
   ],
   imports: [

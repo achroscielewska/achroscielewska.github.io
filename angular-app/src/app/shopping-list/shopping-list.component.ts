8ae5ee733b35d7973eafdbe8156eb982d06427c3
achroscielewska <achroscielewska@gmail.com>
2018-03-26 16:12:22 +0200

    angular-app | shopping component struktura

--- angular-app/src/app/shopping-list/shopping-list.component.ts
+++ angular-app/src/app/shopping-list/shopping-list.component.ts
@@ -9,7 +9,8 @@ import { Ingredient } from '../shared/ingredient.model'
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient [] = [
     new Ingredient('Apples', 5),
-    new Ingredient('Tomatos', 10)
+    new Ingredient('Tomatos', 10),
+    new Ingredient('Orange', 10)
   ];
 
   constructor() { }

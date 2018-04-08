a215ce40999e275a767f64ed0a29b965274493ed
achroscielewska <achroscielewska@gmail.com>
2018-04-08 21:33:06 +0200

    angular-app | shopping-list form

--- angular-app/src/app/shopping-list/shopping-list.component.ts
+++ angular-app/src/app/shopping-list/shopping-list.component.ts
@@ -1,24 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model'
 import { ShoppingListService } from "./shopping-list.service";
+import {Subscription} from "rxjs/Subscription";
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private subsicription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
-    this.shoppingListService.ingredientsChanged
+    this.subsicription = this.shoppingListService.ingredientsChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredients = ingredients;
         }
       )
+  }
+
+  onEditItem(index: number) {
+   this.shoppingListService.startEditing.next(index);
+  }
+
+  ngOnDestroy() {
+    this.subsicription.unsubscribe();
   }
 }

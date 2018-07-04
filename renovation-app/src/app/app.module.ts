c7b344e5ad5094263ae2248aba8d8795e31c2acc
Agnieszka <achroscielewska@gmail.com>
2018-07-04 20:02:09 +0200

    main page box style

--- renovation-app/src/app/app.module.ts
+++ renovation-app/src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MaterializeModule } from 'angular2-materialize'
+
 
 import { AppComponent } from './app.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoomsComponent
   ],
   imports: [
     BrowserModule,
-    //MaterializeModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

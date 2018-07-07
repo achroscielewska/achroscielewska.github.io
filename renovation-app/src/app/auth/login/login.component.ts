2972ca3cec388aba81855c5a93ecf37fd485f6b0
Agnieszka <achroscielewska@gmail.com>
2018-07-07 12:13:54 +0200

    css -> sass + login UI style

--- renovation-app/src/app/auth/login/login.component.ts
+++ renovation-app/src/app/auth/login/login.component.ts
@@ -5,7 +5,7 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.sass']
 })
 export class LoginComponent {
 
--- renovation-app/src/app/components/rooms/rooms.component.ts
+++ renovation-app/src/app/components/rooms/rooms.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
-  styleUrls: ['./rooms.component.css']
+  styleUrls: ['./rooms.component.sass']
 })
 export class RoomsComponent implements OnInit {
 
